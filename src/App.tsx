bba9d08a248582191a4c512ec4300e72782e2c1d
Cipto Hadi <ciptohadi79@gmail.com>
2022-06-30 08:40:28 +0700

    enable preloader & include react-toast css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import { fixAosAnim } from "./utils";
 import { Footer, Header, Main } from "./components";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -19,20 +20,20 @@ function App() {
 
   return (
     <>
-      {/* {loading ? (
+      {loading ? (
         <Preloader />
-      ) : ( */}
-      <div
-        className={
-          themeMode +
-          " w-full h-screen text-gray-800 bg-gray-50 dark:bg-gray-800 dark:text-gray-50"
-        }
-      >
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-      {/*  )} */}
+      ) : (
+        <div
+          className={
+            themeMode +
+            " w-full h-screen text-gray-800 bg-gray-50 dark:bg-gray-800 dark:text-gray-50"
+          }
+        >
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
