883bb4957146859078994967a2d2ef6b79f8399b
Cipto Hadi <ciptohadi79@gmail.com>
2022-07-03 01:10:37 +0700

    fix always reload when no hash

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -10,7 +10,7 @@ export const MainPage = ({ themeMode }: { themeMode: string }) => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
     fixAosFromStarting();
-    window.location.href = window.location.hash; // on window load, scroll page based on hash
+    !!window.location.hash && (window.location.href = window.location.hash); // on window load, scroll page based on hash
   }, []);
 
   return (
